c769a74fd5ce4a5d781e258da901bea73b822742
daniel-tsonkov <daniel_tzonkov@abv.bg>
2024-09-06 22:37:34 +0300

    up

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,7 +39,18 @@ window.addEventListener('scroll', bgHeader);
 bgHeader();
 
 /*=============== SWIPER SERVICES ===============*/
+const swiperServices = new Swiper('.services__swiper', {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 24,
+  slidesPerView: 'auto', 
 
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
 
 /*=============== SHOW SCROLL UP ===============*/
 
